316ad0e73cd28d71243a44e4c3cb85036aed153c
wind111-lang <k20076kk@aitech.ac.jp>
2024-03-07 21:16:15 +0900

    Add Chap3

---
+++ 03/increment.php
@@ -0,0 +1,13 @@
+<?php
+$x = 10;
+$y = ++$x;
+
+print $x . '<br/>';
+print $y . '<br/>';
+
+$m = 10;
+$n = $m++;
+
+print $m . '<br/>';
+print $n;
+?>
---
+++ 03/m_increment.php
@@ -0,0 +1,12 @@
+<?php
+$i = 'Z';
+
+print ++$i . '<br />';
+print ++$i . '<br />';
+
+$j = 'T9';
+
+print ++$j. '<br />';
+print ++$j. '<br />';
+
+?>
