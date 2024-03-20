23a1608dce84bedc426ca1b9833d5c7fed1ab107
wind111-lang <k20076kk@aitech.ac.jp>
2024-03-20 20:38:29 +0900

    Add for func

---
+++ omake/array_for.php
@@ -0,0 +1,19 @@
+<?php
+for ($i = 0; $i < 0; $i++) {
+    print "i = {$i}\n";
+}
+// normal for func
+
+$array = [1, 2, 3, 4, 5];
+
+foreach ($array as $value) {
+    print "value = {$value}\n";
+}
+// normal foreach func
+$array_map = ['Apple' => 'Red', 'Banana' => 'Yellow', 'Grape' => 'Purple'];
+
+foreach ($array_map as $key => $value) {
+    print "key(fruit) = {$key}, value(color) = {$value}\n";
+}
+// foreach func with map
+?>
