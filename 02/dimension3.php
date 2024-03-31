54254c7b979ce28e0fc6fa4700739e1f2d575995
wind111-lang <windmill@pluslab.org>
2024-03-31 14:09:33 +0900

    fix dimension03

--- 02/dimension3.php
+++ 02/dimension3.php
@@ -1,1 +1,14 @@
-c
+<?php
+$data3 = [
+    [
+        'name' => 'Taro',
+        'age' => 22,
+        'gender' => 'male',
+    ],
+    [
+        'name' => 'Jiro',
+        'age' => 23,
+        'gender' => 'male',
+    ],
+]
+?>
