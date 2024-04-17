433eca7b80cc3748cc9cc4eebfe3550c07896701
shota.tsutsui <shota.tsutsui@M24-003-prtadminnoMacBook-Pro.local>
2024-04-17 13:12:34 +0900

    Add preg_split and modifier_*

---
+++ 05/modifier_m.php
@@ -0,0 +1,9 @@
+<?php
+$str = "7人の小人と白雪姫\n101匹ワンちゃん";
+
+if(preg_match_all('/^[0-9]{1,}/m', $str, $data)){
+    foreach($data[0] as $item){
+        print "マッチング結果: {$item} <br />";
+    }
+}
+?>
---
+++ 05/modifier_s.php
@@ -0,0 +1,8 @@
+<?php
+$str = "初めまして. \nよろしくお願いします.";
+if (preg_match_all('/\A.+/s', $str, $data)){
+    foreach($data[0] as $item){
+        print $item;
+    }
+}
+?>
