dbbb0913c87155d9a2229454d4c6714a8e4b6471
wind111-lang <shota.tsutsui@prtimes.co.jp>
2024-04-18 08:59:40 +0900

    add after

--- 05/preg_after.php
+++ 05/preg_after.php
@@ -1,1 +1,6 @@
 <?php
+$str = '<p>サポートサイト<a href="https://wings.msn.to/">https://www.wings.msn.to/</a></p>';
+
+if (preg_match('/<a href="(.+?)">\1<\/a>/', $str, $data)){
+    print htmlspecialchars($data[0]);
+}
